bcd572c4272495f99b0329536df638c03aac28aa
Stephen Byrne <stephen.byrne@measuresforjustice.org>
2019-01-07 20:45:20 -0500

    icon

--- src/main/java/io/mfj/textricator/gui/TextricatorGuiApp.kt
+++ src/main/java/io/mfj/textricator/gui/TextricatorGuiApp.kt
@@ -1,5 +1,13 @@
 package io.mfj.textricator.gui
 
+import javafx.scene.image.Image
+import javafx.stage.Stage
 import tornadofx.*
 
-class TextricatorGuiApp:App(TextricatorGuiView::class)
+class TextricatorGuiApp:App(TextricatorGuiView::class) {
+	override fun start(stage: Stage) {
+		super.start(stage)
+
+		stage.icons += TextricatorGuiApp::class.java.getResourceAsStream("/io/mfj/textricator/gui/textricator-logo-512.png").use { Image(it) }
+	}
+}
