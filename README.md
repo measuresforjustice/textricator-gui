21e4efcf8c6449b32a6d6f58baa22a8d6a17553a
Stephen Byrne <stephen.byrne@measuresforjustice.org>
2019-01-08 20:53:21 -0500

    add license info

---
+++ README.md
@@ -0,0 +1,10 @@
+_Textricator GUI_ is a GUI for [Textricator](https://textricator.mfj.io)
+
+Development is in progress, there have been no releases yet.
+
+_Textricator GUI_ is released under the
+[GNU Affero General Public License Version 3](https://www.gnu.org/licenses/agpl-3.0.en.html).
+
+This application is actively used and developed by [Measures for Justice](https://measuresforjustice.org).
+We welcome feedback, bug reports, and contributions. Create an issue, send a pull request,
+or email us at <textricator@mfj.io>.
--- src/main/java/io/mfj/textricator/gui/AboutView.kt
+++ src/main/java/io/mfj/textricator/gui/AboutView.kt
@@ -12,6 +12,10 @@ class AboutView:View() {
 		vbox {
 			label("Textricator version: ${io.mfj.textricator.Version.version}")
 			label("Textricator GUI version: ${Version.version}")
+			label("Copyright ${Version.copyrightYear} Measures for Justice Institute.")
+			label("Licensed under the GNU Affero General Public License, Version 3.")
+			label("(Loaded modules may be licensed differently.)")
+			label("Source code is available at ${Version.sourceLocation}")
 		}
 		setOnMouseClicked { event -> close() }
 		onDoubleClick { close() }
